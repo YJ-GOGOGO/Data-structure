85a0126eec14ffb4ba8f36d78b75c98abe416c12
1392814139@qq.com <1392814139@qq.com>
2022-10-17 10:44:14 +0800

    修改

---
+++ Algorithm/BF.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#define MaxSize 100
+
+
+// 静态顺序表结构
+typedef struct 
+{
+    int array[MaxSize];
+    int length;
+}tabe;
+
+
+// void InputSL( tabe *p )
+// {
+//     int i, length;
+//     printf("请输入第一串字符串长度：\n");
+//     scanf("%d", &length);
+//     printf("请输入%d个数据，并且空格相隔，回车键表示结束", length);
+//     for (int i = 1; i < length; i++)
+//     {
+//         scanf("%c", &p->array);
+//     }
+//     p -> length = length;
+// }
+
+int mate( char *A, char *B )
+{
+    
+}
+
+
+int main()
+{
+    tabe *p;
+    printf("\t这是一个BF算法\n");
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
